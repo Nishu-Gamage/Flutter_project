f139c4c27e03e90aa88a57580d4e420242d54393
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-11-02 13:18:02 +0900

    [AppBar] - Initialize the AppBar component

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text("Flutter Appbar"),
       ),
     );
   }

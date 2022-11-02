54f9ec6ded18b462c61081b7cf248689a8a94627
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-11-02 14:58:06 +0900

    [AppBar] - flexibleSpace & SafeArea class

--- lib/main.dart
+++ lib/main.dart
@@ -25,23 +25,31 @@ class MyAppBar extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Appbar"),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: (){},
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
+    return SafeArea(
+      minimum:  EdgeInsets.only(top:30),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Appbar"),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
             onPressed: (){},
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: (){},
+          ),
+          ],
+          flexibleSpace: Icon(
+            Icons.photo_camera,
+            size: 75.0,
+            color: Colors.white70,
+          ),
         ),
-        IconButton(
-          icon: Icon(Icons.more_vert),
-          onPressed: (){},
-        ),
-        ],
       ),
     );
   }

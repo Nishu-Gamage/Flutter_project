73ba45d3ca1b3b9d9cb9865db37881c7e636a865
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-11-07 10:45:04 +0900

    [AppBar] - bottom (Tab)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() =>  runApp(
-  DevicePreview(
-    builder: (context) => MyAppBar(),
-  ),
-);
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => MyAppBar(),
+      ),
+    );
 
 class MyAppBar extends StatelessWidget {
   @override
@@ -14,42 +14,62 @@ class MyAppBar extends StatelessWidget {
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       title: 'AppBar',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
-      );
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      minimum:  EdgeInsets.only(top:30),
+    return DefaultTabController(
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: Text("Flutter Appbar"),
           leading: IconButton(
             icon: Icon(Icons.menu),
-            onPressed: (){},
+            onPressed: () {},
           ),
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.search),
-              onPressed: (){},
-          ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: (){},
-          ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
           ],
           flexibleSpace: Image.asset(
-            "assests/back1.jpg", 
+            "assests/back1.jpg",
             fit: BoxFit.cover,
           ),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car), text: 'Tab 1',),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Text("Nishu Gamage Design"),
+            tab1(),
+            Icon(Icons.directions_bike),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget tab1() {
+  return Container(
+    child: Center(
+      child: Text('Bottom AppBar body method display test'),
+    ),
+  );
 }

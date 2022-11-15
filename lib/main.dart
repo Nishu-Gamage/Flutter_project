8ffb1758d9340b2c726e0f33165818193fc6f6e6
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-11-15 14:09:37 +0900

    [Widget] - TextField

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter_project/screens/secound_screen.dart';
 import 'screens/home_page.dart';
 
-void main() {
-  runApp(NishuApp());
-}
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => NishuApp(),
+      ),
+    );
 
 class NishuApp extends StatelessWidget {
   @override
@@ -28,12 +30,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                maxLength: 8,
+                // obscureText: true,
+                // keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 22.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                autofocus: true,
+                // enabled: false,
+                cursorColor: Colors.red,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                ),
+                style: TextStyle(
+                  fontSize: 22.0,
+                ),
+              ),
+            )
+          ],
+        ),  
       ),
     );
   }

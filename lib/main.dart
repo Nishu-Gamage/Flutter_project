a2b0e6cddffd540ddd9d4d978ee8f486e5299c81
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-11-16 14:44:29 +0900

    Asynchronous Programming

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter_project/form/form-test.dart';
 
-import 'form/form.dart';
+import 'asynchronous/network_request.dart';
 
 void main() => runApp(
       DevicePreview(
@@ -16,9 +16,50 @@ class NishuApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FormScreen(),
+      home: Home(),
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Asynchronous Programming"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Asynchronous Programming',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  child: Text('CLICK ME'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NetworkRequest()),
+                    );
+                  }),
+            )
+            // RaisedButton(child: Text('GET DATA'), onPressed: () {})
+          ],
+        ),
+      ),
     );
   }
 }

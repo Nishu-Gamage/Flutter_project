4573e6681299850a976bc570d5f375d023ee66d4
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-11-17 10:02:10 +0900

    [anyce] - Future API

---
+++ lib/asynchronous/main-asynchronous.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter_project/form/form-test.dart';
+
+import 'network_request.dart';
+
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => NishuApp(),
+      ),
+    );
+
+class NishuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Asynchronous Programming"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Asynchronous Programming',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  child: Text('CLICK ME'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NetworkRequest()),
+                    );
+                  }),
+            )
+            // RaisedButton(child: Text('GET DATA'), onPressed: () {})
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter_project/form/form-test.dart';
-
 import 'asynchronous/network_request.dart';
 
 void main() => runApp(
@@ -18,47 +17,6 @@ class NishuApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Home(),
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Asynchronous Programming"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Asynchronous Programming',
-                style: TextStyle(
-                  fontSize: 22,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                  child: Text('CLICK ME'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NetworkRequest()),
-                    );
-                  }),
-            )
-            // RaisedButton(child: Text('GET DATA'), onPressed: () {})
-          ],
-        ),
       ),
     );
   }
